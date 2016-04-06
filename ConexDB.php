acb53dd15d90330916fc135094c7ff5bd378fc1f
ThomasD <dufaux.thom@gmail.com>
2016-04-06 09:38:59 +0200

    Modif Conexion DB pour publication

--- ConexDB.php
+++ ConexDB.php
@@ -2,7 +2,10 @@
 
 <?php
     function ConDB(){
-        $Conex = new PDO("mysql:host=localhost;charset=utf8;dbname=sites", "root", "");
+        //$Conex = new PDO("mysql:host=localhost;charset=utf8;dbname=thomasd", "thomasd", "bigtom19"); //Ces données sont pour le site PUBLIÉ !!!
+
+        $Conex = new PDO("mysql:host=localhost;charset=utf8;dbname=sites", "root", ""); //Et là, pour tourner en local !!!
+
         $Conex->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $Conex;
 
